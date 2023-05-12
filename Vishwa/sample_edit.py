728e51d8ee02d0e6d8353424f01043e55fb810f5
VT597 <65662921+VT597@users.noreply.github.com>
2023-05-12 18:22:40 +0530

    Sample edit 5
    More edits to the code

--- Vishwa/sample_edit.py
+++ Vishwa/sample_edit.py
@@ -1,55 +1,44 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
-# create the root window
 root = tk.Tk()
 root.title("Login Page")
-root.resizable(0, 0)
+root.resizable(0,0)
 root.geometry('1000x562')
 
-back_img = ImageTk.PhotoImage(file="bg1.jpg")
-back_lab = tk.Label(root, image=back_img)
-back_lab.place(x=0, y=0, relwidth=1, relheight=1)
+back_img=ImageTk.PhotoImage(file="bg1.jpg")
 
-# create a frame for the login form
-login_frame = tk.Frame(root, bg=back_lab.cget('background'))
+back_lab = tk.Label(root,image=back_img)
+back_lab.place(x=0,y=0,relwidth=1,relheight=1)
+
+login_frame = tk.Frame(root, bg="white")
 login_frame.pack(expand=True)
 
-# create a label for the username field
-username_label = tk.Label(login_frame, text="Username", bg=back_lab.cget('background'), font=("Arial", 20))
-username_label.grid(row=0, column=0, padx=20, pady=20, sticky="E")
+username_label = tk.Label(login_frame, text="Username", bg="white", font=("Arial", 14))
+username_label.grid(row=0, column=0, padx=10, pady=10, sticky="E")
 
-# create an entry field for the username
-username_entry = tk.Entry(login_frame, width=30, font=("Arial", 20))
-username_entry.grid(row=0, column=1, padx=20, pady=20, sticky="W")
+username_entry = tk.Entry(login_frame, width=20, font=("Arial", 14))
+username_entry.grid(row=0, column=1, padx=10, pady=10, sticky="W")
 
-# create a label for the email field
-email_label = tk.Label(login_frame, text="Email", bg=back_lab.cget('background'), font=("Arial", 20))
-email_label.grid(row=1, column=0, padx=20, pady=20, sticky="E")
+email_label = tk.Label(login_frame, text="Email", bg="white", font=("Arial", 14))
+email_label.grid(row=1, column=0, padx=10, pady=10, sticky="E")
 
-# create an entry field for the email
-email_entry = tk.Entry(login_frame, width=30, font=("Arial", 20))
-email_entry.grid(row=1, column=1, padx=20, pady=20, sticky="W")
+email_entry = tk.Entry(login_frame, width=20, font=("Arial", 14))
+email_entry.grid(row=1, column=1, padx=10, pady=10, sticky="W")
 
-# create a label for the password field
-password_label = tk.Label(login_frame, text="Password", bg=back_lab.cget('background'), font=("Arial", 20))
-password_label.grid(row=2, column=0, padx=20, pady=20, sticky="E")
+password_label = tk.Label(login_frame, text="Password", bg="white", font=("Arial", 14))
+password_label.grid(row=2, column=0, padx=10, pady=10, sticky="E")
 
-# create an entry field for the password
-password_entry = tk.Entry(login_frame, show="*", width=30, font=("Arial", 20))
-password_entry.grid(row=2, column=1, padx=20, pady=20, sticky="W")
+password_entry = tk.Entry(login_frame, show="*", width=20, font=("Arial", 14))
+password_entry.grid(row=2, column=1, padx=10, pady=10, sticky="W")
 
-# create a forgot password button
-forgot_password_button = tk.Button(login_frame, text="Forgot Password", bg=back_lab.cget('background'), fg="blue", font=("Arial", 16), width=15)
-forgot_password_button.grid(row=3, column=0, padx=20, pady=20, sticky="W")
+forgot_password_button = tk.Button(login_frame, text="Forgot Password", bg="white", fg="blue", font=("Arial", 12), width=15)
+forgot_password_button.grid(row=3, column=0, padx=10, pady=10, sticky="W")
 
-# create a login button
-login_button = tk.Button(login_frame, text="Login", bg="blue", fg="white", font=("Arial", 20), width=15, height=2)
-login_button.grid(row=3, column=1, padx=20, pady=20, sticky="E")
+login_button = tk.Button(login_frame, text="Login", bg="blue", fg="white", font=("Arial", 14), width=10, height=1)
+login_button.grid(row=3, column=1, padx=10, pady=10, sticky="E")
 
-# create a sign up button
-sign_up_button = tk.Button(root, text="Sign Up", bg=back_lab.cget('background'), fg="blue", font=("Arial", 16), width=10, height=1)
-sign_up_button.pack(side="bottom", pady=20, padx=20, anchor="se")
+sign_up_button = tk.Button(root, text="Sign Up", bg="white", fg="blue", font=("Arial", 12), width=10, height=1)
+sign_up_button.pack(side="bottom", pady=10, padx=10, anchor="se")
 
-# run the main event loop
 root.mainloop()
