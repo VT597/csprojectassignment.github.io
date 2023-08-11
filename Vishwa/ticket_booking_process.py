9ecf71b1c0d9eeb6e2c9e5116814a7a19205357e
VT597 <65662921+VT597@users.noreply.github.com>
2023-08-11 18:05:37 +0530

    Update ticket_booking_process.py
    THIS CODE IS STILL TO BE EDITED

--- Vishwa/ticket_booking_process.py
+++ Vishwa/ticket_booking_process.py
@@ -1,1 +1,32 @@
+import csv
+import random
+f=open("Ticketdetails.csv",'a')
+writer=csv.writer(f):
+def upper_seat():
+    upperseat=[]
+    for i in range(1,10001):
+        upperseat.append(["S"+str(i)])
+def lower_seat():
+    lowerseat=[]
+    for i in range(1,10001):
+        lowerseat.append(["S"+str(i)])
+    print(upperseat)
+upper_seat()
+def write_file(username,ticket_number,seat_number,stand):
+    
 
+
+
+f.close()
+
+f=open("Ticketdetails.csv",'r')
+reader=csv.reader(f)
+def file_read():
+    ticketcount=0
+    for i in reader:
+        ticketcount+=int(i[2])
+        if i[3]=="Upper stand":
+            book_list=<no.of seats>
+            x=random.choice(upperseat)
+            
+    
