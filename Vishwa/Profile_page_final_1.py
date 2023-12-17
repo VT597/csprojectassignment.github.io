10282b023ab9142920b90deb264be40a93927a3c
VT597 <65662921+VT597@users.noreply.github.com>
2023-12-17 20:53:57 +0530

    Update Profile_page_final_1.py

--- Vishwa/Profile_page_final_1.py
+++ Vishwa/Profile_page_final_1.py
@@ -1,40 +1,44 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import TclError
 
 def open_file_dialog():
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
-    if file_path:
-        pfp_img = PhotoImage(file=file_path)
-        pfp_label.config(image=pfp_img)
-        pfp_label.image = pfp_img  # Keep a reference to avoid garbage collection
+    try:
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
+        if file_path:
+            pfp_img = PhotoImage(file=file_path)
+            pfp_label.config(image=pfp_img)
+            pfp_label.image = pfp_img  # Keep a reference to avoid garbage collection
+    except TclError:
+        messagebox.showerror("Error", "Invalid image file format. Please select a valid image.")
 
 # Create the main window
 profile_window = Tk()
 profile_window.title("Profile Page")
-profile_window.geometry("400x600")
+profile_window.geometry("800x600")
 
 # Profile Picture (PFP)
-pfp_label = Label(profile_window, text="PFP", font=("Arial", 16), relief="solid")
-pfp_label.pack(pady=10)
+pfp_label = Label(profile_window, text="PFP", font=("Arial", 24), relief="solid")
+pfp_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
-select_pfp_button = Button(profile_window, text="Select PFP", command=open_file_dialog)
-select_pfp_button.pack(pady=5)
+select_pfp_button = Button(profile_window, text="Select PFP", command=open_file_dialog, font=("Arial", 16))
+select_pfp_button.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
 # Username (Retrieve from MySQL or use a placeholder)
-username_label = Label(profile_window, text="<USERNAME>", font=("Arial", 14))
-username_label.pack(pady=10)
+username_label = Label(profile_window, text="<USERNAME>", font=("Arial", 24))
+username_label.grid(row=0, column=1, padx=10, pady=10, sticky="e")
 
 # Ticket Details
 ticket_frame = Frame(profile_window, relief="solid", borderwidth=1)
-ticket_frame.pack(pady=20, padx=10)
+ticket_frame.grid(row=2, column=0, columnspan=2, pady=20, padx=10, sticky="w")
 
-date_label = Label(ticket_frame, text="<Date>", font=("Arial", 12))
+date_label = Label(ticket_frame, text="<Date>", font=("Arial", 20))
 date_label.grid(row=0, column=0, pady=5)
 
-month_label = Label(ticket_frame, text="<Month>           Match 07: SS vs RC", font=("Arial", 12))
+month_label = Label(ticket_frame, text="<Month>           Match 07: SS vs RC", font=("Arial", 20))
 month_label.grid(row=1, column=0, pady=5)
 
-year_label = Label(ticket_frame, text="<Year>", font=("Arial", 12))
+year_label = Label(ticket_frame, text="<Year>", font=("Arial", 20))
 year_label.grid(row=2, column=0, pady=5)
 
 # Run the Tkinter event loop
